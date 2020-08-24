8a02ab0a2542409ec87c525d7e8304b74d17a855
Arturo Rendon <arturo@sube.la>
2020-08-23 22:24:55 -0700

    getNewState method added

--- Models/nfa.py
+++ Models/nfa.py
@@ -10,5 +10,24 @@ class NFA(Automata):
 
     # NFA Evaluate method returns the equivalent DFA
     def evaluate(self, expression): 
-        # 
+        # Place the equivalent transitions per state on new DFA automata
+        
         pass
+
+    def getDFATransitions(self):
+        # Get all the equivalent transitions
+        for transition in self.transitions:
+            currentTransitionKeys = list(transition.keys)
+            for aElement in self.alphabet:
+                if(self.transitions[(currentTransitionKeys, aElement)] not in self.transitions):
+                    pass
+                pass
+            pass
+        pass
+
+    def getNewState(self):
+        lastState = self.states[len(self.states)-1]
+        currentNumState = int(lastState[1])
+        currentNumState += 1
+        return "q"+currentNumState
+
