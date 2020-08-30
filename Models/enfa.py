40bf3fbbc5461cd149c9375401d11e754aee2bf1
Arturo rendon <arturo@sube.la>
2020-08-30 00:24:42 -0600

    Completed ENFA to NFA Equivalence

--- Models/enfa.py
+++ Models/enfa.py
@@ -7,22 +7,30 @@ class ENFA(Automata):
 
     # evaluate transforms ENFA to NFA
     def evaluate(self, expression):
-        newNFA = NFA()
+        dbAutomata = dict()
+        dbAutomata["alphabet"] = self.alphabet
+        dbAutomata["states"] = self.states
+        dbAutomata["initial_state"] = self.initial_state
+        newNFA = NFA(dbAutomata, self.accepting_states, self.transitions)
+        newNFA.transitions = dict()
+
+        alphabetList = self.alphabet
+        alphabetList.remove('Ɛ')
+        newNFA.alphabet = alphabetList
+        
         # Define equivalent NFA transitions
         for state in self.states:
-            for aElement in self.alphabet:
-                newNFA.transitions[(state, aElement)] = self.getNextState(state, aElement)
+            for aElement in newNFA.alphabet:
+                nextState = self.getNextState(state, aElement)
+                if nextState != []:
+                    newNFA.transitions[(state, aElement)] = nextState 
 
         # Equal automata elements
         newNFA.states = self.states
         
         newNFA.accepting_states = self.accepting_states
         
-        alphabetList = self.alphabet
-        alphabetList.remove('Ɛ')
-        newNFA = alphabetList
 
-        newNFA.accepting_states = self.accepting_states
         return newNFA
 
     def getNextState(self, state, aElement):
@@ -30,18 +38,18 @@ class ENFA(Automata):
         firstEnclosureList = self.getEpsilonEnclosure(state)
 
         # Get the states you get to in base of the alphabet element and the enclosure element
+        stateFromEnclosureList = list()
         for firstEnclosureElement in firstEnclosureList:
             # Get a list of all transitions in base of the firstEnclosureElements
-            stateFromEnclosureList = list()
             for transition in self.transitions:
-                transitionTupleElements = list(transition.keys())
+                transitionTupleElements = list(transition)
                 if(transitionTupleElements[1] == aElement and firstEnclosureElement == transitionTupleElements[0]):
-                    stateFromEnclosureList.append(transition)
+                    stateFromEnclosureList.extend(self.transitions[transition].split(','))
                     break
         # Second for end
 
         # Get epsilon enclosure per states from the first enclosure
-        if(stateFromEnclosureList.count() > 0):
+        if(len(stateFromEnclosureList) > 0):
             nextStates = list()
             for stateFromEnclosure in stateFromEnclosureList:
                 nextStates.extend(self.getEpsilonEnclosure(stateFromEnclosure))
@@ -51,10 +59,14 @@ class ENFA(Automata):
 
     def getEpsilonEnclosure(self, state):
         firstEnclosureList = list()
-        nextState = state
+        firstEnclosureList.append(state)
         for transition in self.transitions:
-            transitionTupleElements = list(transition.keys())
-            if transitionTupleElements[1] == 'Ɛ' and transitionTupleElements[0] == nextState:
-                firstEnclosureList.append(transition)
-                nextState = transition
+            transitionTupleElements = list(transition)
+            if transitionTupleElements[1] == 'Ɛ' and transitionTupleElements[0] == state:
+                splitTransition = self.transitions[transition].split(',')
+                if(len(splitTransition) > 1):
+                    for split in splitTransition:
+                        firstEnclosureList = firstEnclosureList+self.getEpsilonEnclosure(split)
+                else:
+                    firstEnclosureList = firstEnclosureList+splitTransition
         return firstEnclosureList
