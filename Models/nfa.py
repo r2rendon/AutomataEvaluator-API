9c9e0eb14cd3d098c25c725b50bd2e45e435017b
Arturo rendon <arturo@sube.la>
2020-08-31 03:46:10 -0600

    NFA now translates itself to DFA

--- Models/nfa.py
+++ Models/nfa.py
@@ -4,48 +4,68 @@ from Models.dfa import DFA
 class NFA(Automata):
     def __init__(self, dbAutomata, accepting_states, transitions):
         super().__init__(dbAutomata, accepting_states, transitions)
-        
-    def __init__(self):
-        super().__init__()
 
     # NFA Evaluate method returns the equivalent DFA
     def evaluate(self, expression): 
-        newDFA = DFA()
+        dbAutomata = dict()
+        dbAutomata["alphabet"] = self.alphabet
+        dbAutomata["states"] = self.states
+        dbAutomata["initial_state"] = self.initial_state
+        newDFA = DFA(dbAutomata, self.accepting_states, self.transitions)
 
         newDFA.alphabet = self.alphabet
-        newDFA.states = self.states
 
         newDFA.initial_state = self.initial_state
         newDFA.accepting_states = self.accepting_states
 
         # Place the equivalent transitions per state on new DFA automata
-        equivalentTransitions = self.getDFATransitions()
-        newDFA.transitions = equivalentTransitions[0]
-        if(equivalentTransitions[1] != ""):
-            newDFA.states.append(equivalentTransitions[1])
+        equivalentTransitions = self.getDFAEquivalence()
+        newDFA.transitions = equivalentTransitions[1]
+        newDFA.states = self.getCleanStates(equivalentTransitions[0])
+        newDFA.accepting_states = set(self.getAcceptingStates(equivalentTransitions[0]))
 
         return newDFA
 
-    def getDFATransitions(self):
-        # Get all the equivalent transitions
-        eqTransitionsDict = dict()
-        newDeadState = ""
-        for transition in self.transitions:
-            currentTransitionKeys = list(transition.keys)
+    def getDFAEquivalence(self):
+        newStates = [[self.initial_state]]
+        newTransitions = dict()
+        for newState in newStates:
             for aElement in self.alphabet:
-                if(self.transitions[(currentTransitionKeys[0], aElement)] not in self.transitions):
-                    if(newDeadState==""):
-                        newDeadState = getNewState()
-                    eqTransitionsDict[(newDeadState, aElement)] = newDeadState
-                else:
-                    eqTransitionsDict[(currentTransitionKeys[0], aElement)] = self.transitions[(currentTransitionKeys[0], aElement)]
-        
-        return (eqTransitionsDict, newDeadState)
+                currentTransition = self.transitions[(newState[0], aElement)] 
+                # newTransitions[(newState, aElement)] = self.transitions[(newState, aElement)]                
+                if currentTransition not in newStates:
+                    if len(currentTransition) > 1:
+                        newStates.append(currentTransition)
+                        for alph in self.alphabet:
+                            res = ''
+                            for transition in currentTransition:
+                                if(transition, alph) in self.transitions:
+                                    res = res+(''.join(self.transitions[(transition, alph)]))
+                            newTransitions[newState[0], alph] = res
+                            newTransitions[''.join(currentTransition), alph] = res
+                    else:
+                        newStates.append(currentTransition)
+                        newTransitions[(newState[0], aElement)] = currentTransition[0]
 
+        return (newStates, newTransitions)
 
-    def getNewState(self):
-        lastState = self.states[len(self.states)-1]
-        currentNumState = int(lastState[1])
-        currentNumState += 1
-        return "q"+currentNumState
+    def getAcceptingStates(self, states):
+        acceptingStates = list()
+        for state in states:
+            for tmp in state:
+                if tmp in self.accepting_states:
+                    acceptingStates.append(''.join(state))
+
+        return acceptingStates
+
+    def getCleanStates(self, states):
+        newStates = list()
+        for state in states:
+            if len(state) > 1:
+                newStates.append(''.join(state))
+            else:
+                newStates.append(state[0])
+
+        return newStates
+
 
